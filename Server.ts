ab14ec5446aa9531feedb7b1d8530507f6bbafc4
Ruvim <malyshev.dev@ya.ru>
2022-06-22 21:30:29 +0300

    fix logger smtp

--- Server.ts
+++ Server.ts
@@ -175,14 +175,16 @@ class Server {
                         new Error(globalMessages['smtp.server.client.error.email'])
                     );
                 }
+                logger.smtp(globalMessages['smtp.server.client.successful.email'], {address: address.address});
                 return callback(); // Accept the address
             },
             onConnect(session, callback) {
                 //console.log("Console in onConnect function>>>",session);
                 if (session.remoteAddress.includes('127.0.0.1','0.0.0.0')) {
                     logger.smtp(globalMessages['smtp.server.client.error.localhost'], null);
-                    return callback(new Error(globalMessages['smtp.server.client.error.localhost']));
+                    //return callback(new Error(globalMessages['smtp.server.client.error.localhost']));
                 }
+                logger.smtp(globalMessages['smtp.server.client.successful.localhost'], {session: session});
                 return callback(); // Accept the connection
             },
             onAuth(auth, session, callback) {
@@ -190,7 +192,9 @@ class Server {
                     logger.smtp(globalMessages['smtp.server.client.error.login'], null);
                     return callback(new Error(globalMessages['smtp.server.client.error.login']));
                 }
-                callback(null, { user: { id: session.id, username: 'admin'}}); // where 123 is the user id or similar property
+                let data = { user: { id: session.id, username: 'admin'}};
+                logger.smtp(globalMessages['smtp.server.client.successful.login'], data);
+                callback(null, data); // where 123 is the user id or similar property
             }
         });
 
