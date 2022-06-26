60f9362065a0e9b8331b34441ec18f3f8de8c902
Ruvim <malyshev.dev@ya.ru>
2022-06-26 20:54:24 +0300

    fix smtp conf

--- Server.ts
+++ Server.ts
@@ -32,7 +32,7 @@ import {verifyToken} from "./middleware/jwtAuth";
 /* Session */
 import { sessions } from "./config/store";
 import morganBody from "morgan-body";
-import {smtpConfig} from "./config/smtp.config";
+import {smtpConfig, smtpServerConf} from "./config/smtp.config";
 import {corsConfig} from "./config/cors.config";
 /* rabbit */
 import { Channel, Connection } from 'amqplib';
@@ -182,51 +182,7 @@ class Server {
         let MailParser = this.MailParser;
         let logger = this.logger;
 
-        const server = new this.SMTPServer({
-            key: this.credentials.key,
-            cert: this.credentials.cert,
-            logger: false,
-            authOptional: true,
-            onData(stream, session, callback) {
-                MailParser.simpleParser(stream, (err, main) => {
-                    let data = {
-                        from: main.from.text,
-                        to: main.to.text,
-                        text: main.text
-                    }
-                    logger.smtp(globalMessages['smtp.server.mail.send'], data)
-                    callback(err);
-                });
-            },
-            onMailFrom(address, session, callback) {
-                if (address.address !== "malyshev.dev@ya.ru") {
-                    logger.smtp(globalMessages['smtp.server.client.error.email'], null);
-                    return callback(
-                        new Error(globalMessages['smtp.server.client.error.email'])
-                    );
-                }
-                logger.smtp(globalMessages['smtp.server.client.successful.email'], {address: address.address});
-                return callback(); // Accept the address
-            },
-            onConnect(session, callback) {
-                //console.log("Console in onConnect function>>>",session);
-                if (session.remoteAddress.includes('127.0.0.1','0.0.0.0')) {
-                    logger.smtp(globalMessages['smtp.server.client.error.localhost'], null);
-                    //return callback(new Error(globalMessages['smtp.server.client.error.localhost']));
-                }
-                logger.smtp(globalMessages['smtp.server.client.successful.localhost'], {session: session});
-                return callback(); // Accept the connection
-            },
-            onAuth(auth, session, callback) {
-                if (auth.username !== process.env.SMTP_USER || auth.password !== process.env.SMTP_PASSWORD) {
-                    logger.smtp(globalMessages['smtp.server.client.error.login'], null);
-                    return callback(new Error(globalMessages['smtp.server.client.error.login']));
-                }
-                let data = { user: { id: session.id, username: 'admin'}};
-                logger.smtp(globalMessages['smtp.server.client.successful.login'], data);
-                callback(null, data); // where 123 is the user id or similar property
-            }
-        });
+        const server = new this.SMTPServer(smtpServerConf(this.credentials, MailParser, logger));
 
         server.listen(process.env.SMTP_PORT, async () => {
             if (await this.SMTPTransporter()) {
--- config/smtp.config.ts
+++ config/smtp.config.ts
@@ -1,4 +1,5 @@
 import * as dotenv from 'dotenv';
+import {globalMessages} from "./globalMessages";
 dotenv.config();
 
 export const smtpConfig = {
@@ -14,5 +15,53 @@ export const smtpConfig = {
     },
     tls: {
         rejectUnauthorized: false
+    }
+}
+
+export function smtpServerConf(credentials, MailParser, logger) {
+    return {
+        key: credentials.key,
+        cert: credentials.cert,
+        logger: false,
+        authOptional: true,
+        onData(stream, session, callback) {
+            MailParser.simpleParser(stream, (err, main) => {
+                let data = {
+                    from: main.from.text,
+                    to: main.to.text,
+                    text: main.text
+                }
+                logger.smtp(globalMessages['smtp.server.mail.send'], data)
+                callback(err);
+            });
+        },
+        onMailFrom(address, session, callback) {
+            if (address.address !== "malyshev.dev@ya.ru") {
+                logger.smtp(globalMessages['smtp.server.client.error.email'], null);
+                return callback(
+                    new Error(globalMessages['smtp.server.client.error.email'])
+                );
+            }
+            logger.smtp(globalMessages['smtp.server.client.successful.email'], {address: address.address});
+            return callback(); // Accept the address
+        },
+        onConnect(session, callback) {
+            //console.log("Console in onConnect function>>>",session);
+            if (session.remoteAddress.includes('127.0.0.1','0.0.0.0')) {
+                logger.smtp(globalMessages['smtp.server.client.error.localhost'], null);
+                //return callback(new Error(globalMessages['smtp.server.client.error.localhost']));
+            }
+            logger.smtp(globalMessages['smtp.server.client.successful.localhost'], {session: session});
+            return callback(); // Accept the connection
+        },
+        onAuth(auth, session, callback) {
+            if (auth.username !== process.env.SMTP_USER || auth.password !== process.env.SMTP_PASSWORD) {
+                logger.smtp(globalMessages['smtp.server.client.error.login'], null);
+                return callback(new Error(globalMessages['smtp.server.client.error.login']));
+            }
+            let data = { user: { id: session.id, username: 'admin'}};
+            logger.smtp(globalMessages['smtp.server.client.successful.login'], data);
+            callback(null, data); // where 123 is the user id or similar property
+        }
     }
 }
